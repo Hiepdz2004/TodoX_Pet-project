f4f22b92a1f6b6f49a84102b59704f8f13839319
Hiepdz2004 <lyhiep132@gmail.com>
2026-02-22 22:08:49 +0700

    chuan bi deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,13 @@ import taskRoutes from './routes/taskRoutes.js';
 import { connectDB } from "./config/db.js";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from "path";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -14,9 +17,19 @@ const app = express();
 //middlewares
 app.use(express.json());
 
-app.use(cors({origin: "http://localhost:5173"})); 
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({origin: "http://localhost:5173"})); 
+}
 
 app.use("/api/tasks", taskRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
